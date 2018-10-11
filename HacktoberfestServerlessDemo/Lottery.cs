f345004c78775ca0d9456537427c5cc9eb6940a5
sejka <karol.sejka@gmail.com>
2018-10-12 01:25:00 +0200

    Initial commit

---
+++ HacktoberfestServerlessDemo/Lottery.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace HacktoberfestServerlessDemo
+{
+    public static class Lottery
+    {
+        private const int PROBABILITY = 10000;
+
+        [FunctionName("Lottery")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        {
+            var random = new Random().Next() % PROBABILITY;
+
+            if (random == PROBABILITY - 1)
+            {
+                return new OkObjectResult($"win");
+            }
+            else
+            {
+                return new OkObjectResult($"try again");
+            }
+        }
+    }
+}
